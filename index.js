0780185cd67858446e05ff1602519a320d5115f8
IfeIge <79335183+IfeIge@users.noreply.github.com>
2021-03-22 20:51:15 -0400

    task  1 complete

--- index.js
+++ index.js
@@ -28,8 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+
+  In counter1 the count variable is declared with the local scope of the function.
+
   2. Which of the two uses a closure? How can you tell?
+
+  counter1 uses a closure.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
